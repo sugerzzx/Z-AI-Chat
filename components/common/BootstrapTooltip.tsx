c87ab65ce279bf2c3140fc36d7e1f5087915ad23
Zhou zixuan <sugerzzx@gmail.com>
2024-07-15 22:09:46 +0800

    chore: add common components

---
+++ components/common/BootstrapTooltip.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Tooltip, { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
+
+const BootstrapTooltip = styled(({ className, ...props }: TooltipProps) => <Tooltip {...props} arrow classes={{ popper: className }} />)(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "#0d0d0d",
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "#0d0d0d",
+    fontSize: "1rem",
+    fontWeight: 600,
+    fontFamily: "Inter",
+    padding: "6px 10px",
+    borderRadius: "6px",
+    border: "1px solid #242424",
+  },
+}));
+
+export default BootstrapTooltip;
---
+++ components/common/ClickTrans.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+import { FC, useState } from "react";
+
+interface ClickTransProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const ClickTrans: FC<ClickTransProps> = ({ children, className }) => {
+  const [isTransition, setIsTransition] = useState(false);
+
+  return (
+    <div className={cn("transition-all", className)} onClick={() => setIsTransition(true)} style={{ transform: isTransition ? "scale(0.98)" : "none" }} onTransitionEnd={() => setIsTransition(false)}>
+      {children}
+    </div>
+  );
+};
+
+export default ClickTrans;
