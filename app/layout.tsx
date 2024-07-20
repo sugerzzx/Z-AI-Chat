913899fcbfa7f9b346a1ae7d42e80b2c1423a1be
Zhou zixuan <sugerzzx@gmail.com>
2024-07-20 16:48:35 +0800

    style: add markdowm.css and remove all css files into styles dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import { AppContextProvider } from "@/components/AppContextProvider";
 import SideBar from "@/components/sidebar/Sidebar";
 
