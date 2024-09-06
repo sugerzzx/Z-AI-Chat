163229057e1e7c6a46e51c1779e2ba1de1f14a17
Zhou zixuan <sugerzzx@gmail.com>
2024-09-06 23:51:00 +0800

    adjust and improve Title componet ui, remove setTimeout

--- components/sidebar/Title.tsx
+++ components/sidebar/Title.tsx
@@ -18,47 +18,43 @@ interface TitleProps {
 }
 
 const Title: FC<TitleProps> = ({ id, title, isCurrent }) => {
-  const [isOpen, steIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
 
-  return <li className="relative" style={{ opacity: 1, height: "auto" }}>
+  return <li className="relative h-auto">
     <div className={cn("group relative rounded-lg active:opacity-90", isCurrent || isOpen ? 'bg-secondary' : 'hover:bg-secondary')}>
-      {
-        isEdit ?
-          <div className='p-2'>
-            <Input defaultValue={title} onBlur={() => setIsEdit(false)} autoFocus={true} className='h-auto p-0 focus:border focus:border-chart-2 focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none' />
-          </div>
-          : <>
-            <Link href={`/c/${id}`} className="flex items-center gap-2 p-2">
-              <div className="relative grow overflow-hidden whitespace-nowrap" dir="auto">
-                {title}
-              </div>
-            </Link>
-            <div className={cn("absolute bottom-0 top-0 items-center gap-1.5 pr-2 ltr:right-0 rtl:left-0", isCurrent || isOpen ? 'flex' : 'hidden group-hover:flex')}>
-              <DropdownMenu onOpenChange={(newVal) => {
-                steIsOpen(newVal);
-              }}>
-                <DropdownMenuTrigger asChild>
-                  <Button variant={'ghost'} size={'icon'} className='flex items-center justify-center text-muted-foreground transition hover:text-foreground focus-visible:ring-0'>
-                    <ArrowTooltip title="选项" side="top">
-                      <Ellipsis size={20} />
-                    </ArrowTooltip>
-                  </Button>
-                </DropdownMenuTrigger>
-                {
-                  isOpen ? <DropdownMenuContent align="start">
-                    <DropdownMenuItem className='cursor-pointer' onClick={() => setTimeout(() => setIsEdit(true))}>
-                      编辑
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className='cursor-pointer text-destructive data-[highlighted]:text-destructive'>
-                      删除
-                    </DropdownMenuItem>
-                  </DropdownMenuContent> : <></>
-                }
-              </DropdownMenu>
+      <Link href={`/c/${id}`} className="flex items-center gap-2 p-2">
+        <div className="relative grow overflow-hidden whitespace-nowrap" dir="auto">
+          {title}
+        </div>
+      </Link>
+      <div className={cn("absolute bottom-0 top-0 items-center gap-1.5 pr-2 ltr:right-0 rtl:left-0", isCurrent || isOpen ? 'flex' : 'hidden group-hover:flex', isEdit && 'flex left-0')}>
+        {
+          isEdit ?
+            <div className='absolute bottom-0 left-[7px] right-2 top-0 flex items-center'>
+              <Input defaultValue={title} onBlur={() => setIsEdit(false)} autoFocus={true} className='h-auto w-full p-0 focus:border focus:border-chart-2 focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none' />
             </div>
-          </>
-      }
+            :
+            <DropdownMenu onOpenChange={(newVal) => setIsOpen(newVal)}>
+              <DropdownMenuTrigger asChild>
+                <Button variant={'ghost'} size={'icon'} className='flex items-center justify-center text-muted-foreground transition hover:text-foreground focus-visible:ring-0'>
+                  <ArrowTooltip title="选项" side="top">
+                    <Ellipsis size={20} />
+                  </ArrowTooltip>
+                </Button>
+              </DropdownMenuTrigger>
+              {
+                <DropdownMenuContent align="start" className={isOpen ? '' : 'opacity-0'} onCloseAutoFocus={() => setIsOpen(false)}>
+                  <DropdownMenuItem className='cursor-pointer' onClick={() => setIsEdit(true)}>
+                    编辑
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className='cursor-pointer text-destructive data-[highlighted]:text-destructive'>
+                    删除
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              }
+            </DropdownMenu>}
+      </div>
     </div>
   </li>;
 };
